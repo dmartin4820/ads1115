1ea31b49f4f271279d6eaf041bc33c714f5a781d
U-LAPTOP-6FAFQHTD\Denzal <dom4822@yahoo.com>
2021-06-12 17:24:36 -0700

    modified .ino file so that arduino serial prints voltages on ADC inputs. modified read_data.py to plot output from arduino.

--- read_data.ino/read_data.ino.ino
+++ read_data.ino/read_data.ino.ino
@@ -7,18 +7,28 @@ void setup() {
 
   //Default ADC range set to +/- 6.144 (1 bit = 0.1875mV/ADS1115)
 
+  if (!ads.begin()) {
+    Serial.println("Did not initialize ADS.");
+    while(1);
+  }
 }
 
 void loop() {
   int16_t A0, A1, A2, A3;
+  float V0, V1, V2, V3;
 
-  A0 = ads.computeVolts(A0);
-  A1 = ads.computeVolts(A1);
-  A2 = ads.computeVolts(A2);
-  A3 =ads.computeVolts(A3);
+  A0 = ads.readADC_SingleEnded(0);
+  A1 = ads.readADC_SingleEnded(1);
+  A2 = ads.readADC_SingleEnded(2);
+  A3 = ads.readADC_SingleEnded(3);
+  
+  V0 = ads.computeVolts(A0);
+  V1 = ads.computeVolts(A1);
+  V2 = ads.computeVolts(A2);
+  V3 = ads.computeVolts(A3);
 
-  Serial.print(A0); Serial.print(", "); Serial.print(A1); Serial.print(", "); Serial.print(A2); Serial.print(", "); Serial.println(A3);
+  Serial.print(V0); Serial.print(", "); Serial.print(V1); Serial.print(", "); Serial.print(V2); Serial.print(", "); Serial.println(V3);
 
-  delay(1160); //Max sample rate of ADC
+  delay(1000);
 
 }
--- read_data.py
+++ read_data.py
@@ -1,5 +1,38 @@
 import serial
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+A0 = []
+A1 = []
+A2 = []
+A3 = []
+
 
 arduino = serial.Serial('COM5', 9600)
 
 
+def plot_data(i):
+    data = arduino.readline()
+    data = data.rstrip()
+    data = data.decode('utf-8')
+
+    parsed_data = data.split(',')
+    
+    A0.append(float(parsed_data[0]))
+    A1.append(float(parsed_data[1]))
+    A2.append(float(parsed_data[2]))
+    A3.append(float(parsed_data[3]))
+    
+    ax.clear()
+    
+    ax.plot(A0)
+    ax.plot(A1)
+    ax.plot(A2)
+    ax.plot(A3)
+
+    plt.ylabel('Volts (V)')
+
+ani = animation.FuncAnimation(fig, plot_data, interval=1000)
+plt.show()
